c58486e56bf1c901b20e546fd1a93ecb4475cdad
Rajesh <rajesh786ravi@gmail.com>
2025-03-07 13:53:28 +0530

    User name - caps-small issue fix

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,7 +16,7 @@ public class AccountController(DataContext context) : BaseApiController
         using var hmac = new HMACSHA512();
         var user = new AppUser
         {
-            UserName = registerDTO.UserName.ToLower(),
+            UserName = registerDTO.UserName,
             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
             PasswordSalt = hmac.Key
         };
