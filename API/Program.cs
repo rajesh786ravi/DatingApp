ad108d71144561e65f6b3c8abd94b3c81ddeacc7
Rajesh <rajesh786ravi@gmail.com>
2025-06-19 20:26:25 +0530

    Commented

--- API/Program.cs
+++ API/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers();  //Just adding the controller
 
 // Add DbContext to the container.
 builder.Services.AddDbContext<DataContext>(opt =>
