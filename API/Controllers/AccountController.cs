e262633d9c305651388c5782887456487f9bdb85
Rajesh <rajesh786ravi@gmail.com>
2025-07-07 11:50:01 +0530

    Four

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -143,7 +143,7 @@ public class ExampleDelegateService
     }
     public void delegateFunc(int a)
     {
-        Console.WriteLine(a + a + a);
+        Console.WriteLine(a + a + a + a);
     }
     public void callingFunc()
     {
